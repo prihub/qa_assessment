a65caa0cb582cb26fb6682e2cec8c263b8d06d32
prihub <piyapatel6687@gmail.com>
2026-04-07 14:30:37 +0300

    Activity 1
    Week 2 day 1 activity 1 done

---
+++ Week2-day1/src/main/java/com/bootcampexcercise/modul2/Week2/Day1/activity/VariableAssignmentActivity.java
@@ -0,0 +1,12 @@
+package com.bootcampexcercise.modul2.Week2.Day1.activity;
+
+public class VariableAssignmentActivity {
+public static void main(String[] args){
+    int i=10;
+    int j=10;
+    int sum = i+j;
+    System.out.println("Sum of i and j is" +i +j);
+    System.out.println("Sum of i and j is" +(i+j));
+    System.out.println("Sum of i and j is"+ sum);
+}
+}
