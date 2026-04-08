b2b76bec0290ceae82e6a1fe2835e3e2c3402e06
prihub <piyapatel6687@gmail.com>
2026-04-08 09:42:19 +0300

    Module 2 activities

---
+++ JavaWeek/src/main/java/com/bootcampexcercise/modul2/activity/PrintNumWithWhile.java
@@ -0,0 +1,14 @@
+package com.bootcampexcercise.modul2.activity;
+
+public class PrintNumWithWhile {
+    public static void main(String[] args) {
+
+        int n = 1;
+
+        // While loop to print odd numbers less than 100
+        while (n < 100) {
+            System.out.println(n);
+            n = n + 2;
+        }
+    }
+}
